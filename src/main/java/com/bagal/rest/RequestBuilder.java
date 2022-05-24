7ae8fa2033875841006877fc28c3c7b98082d263
bagalumaji <bagalumaji@gmail.com>
2022-05-24 14:57:12 +0530

    added RequestBuilder class for Rest method

--- src/main/java/com/bagal/rest/RequestBuilder.java
+++ src/main/java/com/bagal/rest/RequestBuilder.java
@@ -6,8 +6,8 @@ import static com.bagal.config.ReqResConfigFactory.getConfig;
 import static com.bagal.reports.ExtentLogger.info;
 import static io.restassured.RestAssured.given;
 
-public final class Rest {
-    private Rest(){}
+public final class RequestBuilder {
+    private RequestBuilder(){}
 
     public static Response POST(Object object){
         info("base uri : "+getConfig().baseURI());
